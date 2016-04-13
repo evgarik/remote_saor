b7132855e958cffa66dc226f48b37fd64584438c
alex.sosov <alex.sosov@hotmail.com>
2016-04-13 16:39:56 +0300

    init

---
+++ app/src/main/java/ru/mts/remote_saor/MainActivity.java
@@ -0,0 +1,13 @@
+package ru.mts.remote_saor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
